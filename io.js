1333e2632d34136e90ed8e81218a707c6a2eed23
oovaa <omarvx211@gmail.com>
2024-06-12 10:11:40 +0200

    refactor: Add terminal option to readline interface in io.js

--- io.js
+++ io.js
@@ -7,7 +7,8 @@ import { createInterface } from 'readline'
  */
 const rl = createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: true
 })
 
 export { rl }
