fdc49cd85a12051e935096ecc87106b0e43a2a52
oovaa <omarvx211@gmail.com>
2024-09-23 08:01:25 +0200

    renamed to handle_args

--- nonInteractive.js
+++ nonInteractive.js
@@ -5,7 +5,7 @@ import { answer_chain } from './commandr'
  * @param { string[] } args
  * @returns  string
  */
-export async function handle_non_interactive(args) {
+export async function handle_args(args) {
   const question = args.slice(2).join(' ')
 
   let ans = await answer_chain.invoke({ question: question, history: '' })
